575f5b5130b439825f35778cc9b6cf3f04134712
Yash Kanodia <yashkanodia16@gmail.com>
2020-09-21 18:24:50 -0400

    Leetcode 1004

---
+++ Max_Consecutive_ones_III.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestOnes(int[] A, int K) {
+        int zeroCount=0;
+        int start=0,res=0;
+        for(int end=0;end<A.length;end++){
+            if(A[end]==0)
+                zeroCount++;
+            if(zeroCount>K){
+                if(A[start]==0)
+                    zeroCount--;
+                start++;
+            }
+            res=Math.max(res,end-start+1);
+        }    
+        return res;
+    }
+}
+
